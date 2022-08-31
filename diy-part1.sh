ef1f008aefb761b40f5722dcfbf7c272a8f18078
copper300 <gaga1080@gmail.com>
2022-09-01 00:12:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,7 +20,7 @@
 # sed -i '$a src-git fw876 https://github.com/fw876/helloworld' feeds.conf.default
 
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git small https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
 #sed -i '$a src-git packages https://git.openwrt.org/feed/packages' feeds.conf.default
 #sed -i '$a src-git luci https://git.openwrt.org/project/luci' feeds.conf.default
